a245483fc8ad002c32fdc751e602d1309035af10
pandeyshivam21197 <35452569+pandeyshivam21197@users.noreply.github.com>
2018-07-16 13:10:34 +0530

    creating javascript file

---
+++ vjs.js
@@ -0,0 +1,4 @@
+var newTask=document.getElementById("new-task");
+var addbutton=document.getElementsByTagName("button")[0];
+var incompletelist=document.getElementById("incomplete-tasks");
+var completelist=document.getElementById("completed-taks");
